8996cd6d1451742a5076cc2c3db61a9cea204114
Samir Rolemberg <sasantos@uolinc.com>
2018-10-11 15:39:17 -0300

    move android x

---
+++ app/src/main/java/br/com/srolemberg/synchro/feat/addfeed/AddFeedViewModel.kt
@@ -0,0 +1,12 @@
+package br.com.srolemberg.synchro.feat.addfeed
+
+import androidx.lifecycle.ViewModel
+
+class AddFeedViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+    var x = 0
+
+    fun teste() {
+        //
+    }
+}
